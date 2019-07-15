53dbda81d145876756572b003b0b518bb3aa5b5d
harbrahm <48707113+harbrahm@users.noreply.github.com>
2019-07-15 13:36:10 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -15,3 +15,8 @@ provider "kubernetes" {
   version = "~> 1.1"
 }
 
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
