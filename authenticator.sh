04ebc7bc1efa7574b94996f26ff6123ec584fb53
harbrahm <48707113+harbrahm@users.noreply.github.com>
2019-07-15 12:33:53 +0530

    Create authenticator.sh

---
+++ authenticator.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Retrieve token with Heptio Authenticator
+TOKEN=$(aws-iam-authenticator token -i $cluster_name | jq -r .status.token)
+
+# Output token as JSON
+jq -n --arg token "$TOKEN" '{"token": $token}'
