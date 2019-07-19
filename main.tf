31bd19e73b86e7c4ffaf3911d25e3f75c7ab368d
harbrahm <48707113+harbrahm@users.noreply.github.com>
2019-07-19 12:47:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,10 +7,13 @@ resource "null_resource" "init_helm" {
 resource "null_resource" "authorize_helm" {
   provisioner "local-exec" {
     command = <<EOT
+      helm init --upgrade
       kubectl create serviceaccount --namespace kube-system tiller
       kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
       kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
-    
+      export HELM_HOST=":44134"
+      tiller -listen ${HELM_HOST} -alsologtostderr >/dev/null 2>&1 &
+ 
 EOT
 
 }
